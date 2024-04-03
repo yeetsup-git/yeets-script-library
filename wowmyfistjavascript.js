577101a79f9c442554f00cc6adbf6cebb951a5a6
Yeetsup <105453143+yeetsup-git@users.noreply.github.com>
2024-04-03 15:19:51 -0600

    Update wowmyfistjavascript.js

--- wowmyfistjavascript.js
+++ wowmyfistjavascript.js
@@ -4,9 +4,9 @@ if (hour < 12) {
   document.getElementById("timelogo").innerHTML = "memes in the morning be like:";
 }
 
-if (hour 12 >) {
+if (hour > 12) {
   document.getElementById("timelogo").innerHTML = "yeet sez: good afternoon";
 }
-if (hour 18 >) {
+if (hour > 18) {
   document.getElementById("timelogo").innerHTML = "yeet's w evening memes";
 }
