465ee50897e049f8054090dacf4b7413b5caf0d2
Yeetsup <105453143+yeetsup-git@users.noreply.github.com>
2024-04-03 15:00:30 -0600

    Update wowmyfistjavascript.js

--- wowmyfistjavascript.js
+++ wowmyfistjavascript.js
@@ -1,5 +1,8 @@
 //Wow my first java script (aint no way 💀)
 
 if (hour < 18) {
-  greeting = "Good day";
+  document.getElementById("timelogo").innerHTML = "yeet sez: good afternoon";
+}
+if (hour 18 >) {
+  document.getElementById("timelogo").innerHTML = "";
 }
