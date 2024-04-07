4b7250f75905f649cec5b93606e6ec0ee876df6d
Yeetsup <105453143+yeetsup-git@users.noreply.github.com>
2024-04-06 18:36:04 -0600

    Update omgmyfirstbashscript.sh

--- omgmyfirstbashscript.sh
+++ omgmyfirstbashscript.sh
@@ -1,10 +1,41 @@
 #!/bin/sh
-# we're gonna start with a simple calculator:
-echo "Enter a number"
-read a
-echo "Enter another number"
-read b
-echo "adding..."
-sum=$((a+b))
+# we're gonna start with a simple calculator (see github commits)
+# echo "Enter a number"
+# read a
+# echo "Enter another number"
+# read b
+# echo "adding..."
+# sum=$((a+b))
+# echo $sum
+#
+#
+#================
+#Yeets calculator
+#================
+echo "Type a number:"
+read num1
+topnum=($num1)
+echo "would you like to divide, subtract, multiply, or add?"
+read symb
+if [ ($symb)="divide" ]
+then
+    mads="/"
+fi
+if [ ($symb)="subtract" ]
+then
+    mads="-"
+fi
+if [ ($symb)="multiply" ]
+then
+    mads="*"
+fi
+if [ ($symb)="add" ]
+then
+    mads="+"
+fi
+echo "type another number"
+read num2
+botnum=($num2)
+echo "calculating.."
+sum=(($topnum $mads $num2))
 echo $sum
-
